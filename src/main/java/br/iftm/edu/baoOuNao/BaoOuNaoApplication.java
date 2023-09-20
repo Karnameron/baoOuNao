807a736279d86f706e8d181a6e8aba9ab38d2d14
Lucas Rocha Andrade <62523002+Karnameron@users.noreply.github.com>
2023-09-20 19:45:47 -0300

    inicio do projeto
    Commitando a base do projeto para inicio das atividades

---
+++ src/main/java/br/iftm/edu/baoOuNao/BaoOuNaoApplication.java
@@ -0,0 +1,13 @@
+package br.iftm.edu.baoOuNao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaoOuNaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaoOuNaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/iftm/edu/baoOuNao/BaoOuNaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.iftm.edu.baoOuNao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaoOuNaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
