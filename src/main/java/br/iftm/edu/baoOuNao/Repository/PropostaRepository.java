ad5a5677852b87d5447816bc82715309adab1fb0
Lucas Rocha Andrade <rochaandradel48@gmail.com>
2023-11-24 22:54:52 -0300

    Adicionando configuração de CORS, regras de negócio referentes à proposta e filtros de conteúdo.

--- src/main/java/br/iftm/edu/baoOuNao/Repository/PropostaRepository.java
+++ src/main/java/br/iftm/edu/baoOuNao/Repository/PropostaRepository.java
@@ -5,7 +5,13 @@ import br.iftm.edu.baoOuNao.domain.model.proposta.Proposta;
 import br.iftm.edu.baoOuNao.domain.model.usuario.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PropostaRepository extends JpaRepository<Proposta, Long> {
     int countPropostaByUsuarioAndCategoria(Usuario usuario, Categoria categoria);
     int countPropostaByUsuario(Usuario usuario);
+
+    List<Proposta> findAllByUsuario(Usuario usuario);
+    List<Proposta> findAllByCategoria(Categoria categoria);
+
 }
